774ed323a0235248e67b40e82ce09fbc40619ed9
mary.vtyurina <mary.vtyurina@gmail.com>
2016-12-20 17:08:03 +0300

    add Show

--- dataDescription.hs
+++ dataDescription.hs
@@ -9,6 +9,12 @@ type Description = String
 type Login = String
 type Pwd = String
 
-data Recipe = Recipe IdUser Rating Name Ingredients Time Description
+data Recipe = Recipe IdUser Rating Name Ingredients Time Description deriving Show
+<<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> origin/master
+=======
+>>>>>>> origin/master
 data User = User IdUser Login Pwd
